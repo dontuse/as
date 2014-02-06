8d94acc945043ed50703ed27922295de04aac052
dontuse <dontusecascade@gmail.com>
2014-02-06 17:45:20 +0600

    in 4

--- html/js/all.js
+++ html/js/all.js
@@ -1,1 +1,29 @@
 var glb = {};
+
+
+
+
+
+$(function () {
+    var $files = $('.js-file');
+
+    if ($files.length === 0) {
+        return;
+    }
+
+    $files.find('input[type="file"]').on('change', function (e) {
+        var val = $(this).val();
+        //  console.log('val',$(this).val());
+        $('.b-input__f-text').text(val);
+    });
+
+    $files.find('input[type="file"]').click(function (e) {
+        e.stopPropagation();
+    });
+    $files.click(function (e) {
+        console.log(e);
+        e.preventDefault();
+        $(this).find("input[type=\"file\"]").click();
+    });
+
+});
