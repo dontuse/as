7fd829d4f0eb2f86c6d3633304d29609cf636e79
dontuse <dontusecascade@gmail.com>
2014-02-10 14:03:07 +0600

    работа работа работа !!

--- html/publish/script.js
+++ html/publish/script.js
@@ -3672,6 +3672,22 @@
 
 }(jQuery, document, window));
 
+/*
+	jQuery Colorbox language configuration
+	language: Russian (ru)
+	translated by: Marfa
+	site: themarfa.name
+*/
+jQuery.extend(jQuery.colorbox.settings, {
+	current: "изображение {current} из {total}",
+	previous: "назад",
+	next: "вперёд",
+	close: "закрыть",
+	xhrError: "Не удалось загрузить содержимое.",
+	imgError: "Не удалось загрузить изображение.",
+	slideshowStart: "начать слайд-шоу",
+	slideshowStop: "остановить слайд-шоу"
+});
 var glb = {};
 
 
